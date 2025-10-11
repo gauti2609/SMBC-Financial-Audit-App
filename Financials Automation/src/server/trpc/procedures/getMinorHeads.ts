be3b33f5813e84a28308952498dea8f302733f15
gauti2609 <cagautam86@gmail.com>
2025-10-12 01:29:07 +0530

    Delete Financials Automation directory

--- Financials Automation/src/server/trpc/procedures/getMinorHeads.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "~/server/db";
-import { baseProcedure } from "~/server/trpc/main";
-
-export const getMinorHeads = baseProcedure.query(async () => {
-  return await db.minorHead.findMany({
-    orderBy: { name: 'asc' },
-  });
-});
